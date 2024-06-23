023e206abad12fdd92cc6e45f679d85fb8248dd2
Nicola Spadari <NicolaSpadari@users.noreply.github.com>
2024-06-23 19:10:39 +0200

    Fix layer alerts

--- layer-alerts/nuxt.config.ts
+++ layer-alerts/nuxt.config.ts
@@ -4,30 +4,9 @@ export default defineNuxtConfig({
 		"@unocss/nuxt",
 		"@nuxt/eslint"
 	],
-	app: {
-		head: {
-			title: "Nuxt 3 Layers",
-			charset: "utf-8",
-			viewport: "width=device-width, initial-scale=1",
-			meta: [
-				{ name: "format-detection", content: "no" }
-			],
-			bodyAttrs: {
-				class: "font-text antialiased"
-			},
-			noscript: [
-				{ children: "JavaScript is required to run this project" }
-			]
-		}
-	},
 	css: [
 		"@unocss/reset/tailwind.css"
 	],
-	vue: {
-		compilerOptions: {
-			isCustomElement: (tag: string) => tag.startsWith("i-")
-		}
-	},
 	eslint: {
 		config: {
 			standalone: false
