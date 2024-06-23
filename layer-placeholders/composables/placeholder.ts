c0a738aea494c361791cc066df03d84069c75b4d
Nicola Spadari <NicolaSpadari@users.noreply.github.com>
2024-06-23 20:18:30 +0200

    Update placeholder naming

--- layer-placeholders/composables/placeholder.ts
+++ layer-placeholders/composables/placeholder.ts
@@ -1,14 +1,14 @@
 export const usePlaceholder = () => {
-	const genericPlaceholder = "https://via.placeholder.com/500";
+	const generic = "https://via.placeholder.com/500";
 
-	const getPlaceholder = (w: number, h = w, text = "") => {
+	const generatePlaceholder = (w: number, h = w, text = "") => {
 		if (text !== "")
 			return `https://via.placeholder.com/${w}x${h}?text=${text}`;
 		return `https://via.placeholder.com/${w}x${h}`;
 	};
 
 	return {
-		genericPlaceholder,
-		getPlaceholder
+		generic,
+		generatePlaceholder
 	};
 };
